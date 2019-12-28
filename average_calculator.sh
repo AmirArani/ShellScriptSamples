1de6865c60367950949eb9a97ba65d4788027b14
Amir Arani <blackstar.m77@gmail.com>
2019-12-28 23:52:00 +0000

    Update average_calculator.sh

--- average_calculator.sh
+++ average_calculator.sh
@@ -1,14 +1,14 @@
 sum=0
 
 # bc is bash calculator
-# $* return all args as a arg
+# $* return all args as a single arg
 # $@ return every arg as a seperate arg
 for i in "$@"
 do
 	sum=`echo "$sum+$i"|bc`
 done
 
-# -l for float
+# bc -l for float
 avg=`echo "$sum/$#" | bc -l`
 
 printf '%0.2f\n' "$avg"
