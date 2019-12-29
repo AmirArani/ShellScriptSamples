d0f5f9d6329688b3bdc9ec4e0ea60410bca0b9b0
Amir Arani <blackstar.m77@gmail.com>
2019-12-29 00:00:37 +0000

    Update average_calculator.sh

--- average_calculator.sh
+++ average_calculator.sh
@@ -1,6 +1,9 @@
+# you can calculate average of n numbers as argumans of this file.
+# for example if you run this file as "./average_calculator.sh 3 5 9" the result will be "5.66"
+
 sum=0
 
-# bc is bash calculator
+# bc is bash calculator. we use it for mathematical phrases
 # $* return all args as a single arg
 # $@ return every arg as a seperate arg
 for i in "$@"
@@ -8,7 +11,7 @@ do
 	sum=`echo "$sum+$i"|bc`
 done
 
-# bc -l for float
+# bc -l for float processing
 avg=`echo "$sum/$#" | bc -l`
 
 printf '%0.2f\n' "$avg"
